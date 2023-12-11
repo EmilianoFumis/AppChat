0bea14e851c77eff5d669c761ce8b2e870bc378f
EmiF <67598179+EmilianoFumis@users.noreply.github.com>
2023-12-11 20:31:24 -0300

    Controladores utilizando FireBase

---
+++ ChatViewController.swift
@@ -0,0 +1,152 @@
+
+import UIKit
+import FirebaseCore
+import FirebaseFirestore
+import FirebaseAuth
+
+
+class ChatViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var messageTextfield: UITextField!
+    
+    //Referencia a Firestore
+    
+    let db = Firestore.firestore()
+    
+    var messages: [Message] = []
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.dataSource = self
+        title = K.appName
+        navigationItem.hidesBackButton = true
+        
+        tableView.register(UINib(nibName: K.cellNibName, bundle: nil), forCellReuseIdentifier:
+        K.cellIdentifier)
+        
+     loadMessages()
+
+    }
+    
+    func loadMessages(){
+        
+        db.collection(K.FStore.collectionName)
+            .order(by: K.FStore.dateField)
+            .addSnapshotListener{ (querySnapshot, error) in
+            
+            self.messages = []
+            
+            if let e = error{
+                print("Ocurrió un error recuperando mensajes. \(e)")
+            }else{
+                
+                if let snapshotDocuements = querySnapshot?.documents{
+                    for doc in snapshotDocuements{
+                        let data = doc.data()
+                        if let messageSender = data[K.FStore.senderField] as? String, let messageBody =
+                            data[K.FStore.bodyField] as? String{
+                            
+                            let newMessage = Message(sender: messageSender, body: messageBody)
+                            
+                            self.messages.append(newMessage)
+                            
+                            DispatchQueue.main.async {
+                                self.tableView.reloadData()  //Actualizar vista de tabla
+                                
+                                //Para que se muestre el útimo mensaje enviado en pantalla
+                                let indexPath = IndexPath(row: self.messages.count - 1, section: 0)
+                                self.tableView.scrollToRow(at: indexPath, at: .top, animated: true)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    @IBAction func sendPressed(_ sender: UIButton) {
+        
+        //Si existe texto y si el remitente tiene correo electrónico
+        
+        if let messageBody = messageTextfield.text, let messageSender = Auth.auth().currentUser?.email{
+          
+            db.collection(K.FStore.collectionName).addDocument(data:
+            [K.FStore.senderField: messageSender, K.FStore.bodyField: messageBody, K.FStore.dateField: Date().timeIntervalSince1970])
+            {(error) in
+                if let e = error{
+                    print("Existe error guardando información en firestore, \(e)")
+                }else{
+                    print("Guardado corretamente de información")
+                    
+                    //Como un proceso de fondo
+                    DispatchQueue.main.async {
+                        self.messageTextfield.text = ""
+                    }
+                }
+                
+            }
+        }
+    }
+    
+    @IBAction func logOutPressed(_ sender: UIBarButtonItem) {
+        
+
+       do {
+         try Auth.auth().signOut()
+           navigationController?.popToRootViewController(animated: true)
+           
+       } catch let signOutError as NSError {
+         print("Error signing out: %@", signOutError)
+       }
+        
+    }
+    
+}
+
+extension ChatViewController: UITableViewDataSource{
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    
+    //Este método se llama tantas veces cómo filas exista
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let message = messages[indexPath.row]
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as! MessageCell
+        
+        cell.label.text = message.body
+        
+        //Este es un mensaje del usuario actual
+        if message.sender == Auth.auth().currentUser?.email{
+            
+            cell.leftImageView.isHidden = true
+            cell.rightImageView.isHidden = false
+            cell.messageBubble.backgroundColor = UIColor(named: K.BrandColors.lightPurple)
+            cell.label.textColor = UIColor(named: K.BrandColors.purple)
+            
+        }
+        //Este es el mensaje de otro usuario
+        else{
+            
+            cell.leftImageView.isHidden = false
+            cell.rightImageView.isHidden = true
+            cell.messageBubble.backgroundColor = UIColor(named: K.BrandColors.purple)
+            cell.label.textColor = UIColor(named: K.BrandColors.lightPurple)
+            
+        }
+        
+        return cell
+    }
+         
+}
+
+
+
---
+++ WelcomeViewController.swift
@@ -0,0 +1,44 @@
+
+//
+
+import UIKit
+import CLTypingLabel
+
+class WelcomeViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: CLTypingLabel!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = false
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        titleLabel.text = "     📱eChat"
+
+       /* titleLabel.text = ""
+        var charIndex = 0.0
+        let titleText = "⚡️FlashChat"
+        for letter in titleText{
+            print("-")
+            print(0.1 * charIndex)
+            print(letter)
+            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { (timer) in self.titleLabel.text?.append(letter) }
+        
+            charIndex += 1
+        }
+        
+      */
+    }
+    
+
+}
